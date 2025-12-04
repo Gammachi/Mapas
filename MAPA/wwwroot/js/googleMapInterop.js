7ccefcf08bd32aceda1eda51e3baa1c198abbc2b
Gammachi <gisrael1509@gmail.com>
2025-12-04 17:24:12 -0600

    cambios master maps

---
+++ MAPA/wwwroot/js/googleMapInterop.js
@@ -0,0 +1,68 @@
+﻿window.geoMap = window.geoMap || {};
+
+window.geoMap.initMap = function (mapElement, centerLat, centerLng, zoom, points) {
+    if (!window.google || !google.maps) {
+        console.error("Google Maps JS API no está cargado");
+        return;
+    }
+
+    // Si ya hay un mapa en ese div, lo “limpiamos”
+    if (mapElement._google_map) {
+        mapElement._google_map = null;
+    }
+
+    const center = { lat: centerLat, lng: centerLng };
+
+    const map = new google.maps.Map(mapElement, {
+        center: center,
+        zoom: zoom,
+        styles: googleMapDarkStyles // tema oscuro (definido abajo)
+    });
+
+    mapElement._google_map = map;
+
+    // Dibujar puntos (marcadores)
+    if (points && points.length > 0) {
+        points.forEach(p => {
+            const marker = new google.maps.Marker({
+                position: { lat: p.lat, lng: p.lng },
+                map: map,
+                title: p.label || ""
+            });
+
+            if (p.label) {
+                const info = new google.maps.InfoWindow({
+                    content: `<div>${p.label}</div>`
+                });
+
+                marker.addListener("click", () => info.open(map, marker));
+            }
+        });
+    }
+};
+// Tema oscuro clásico tipo “Google Night”
+const googleMapDarkStyles = [
+    { elementType: "geometry", stylers: [{ color: "#1d1d1d" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#b0b0b0" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#000000" }] },
+    {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#4a4a4a" }]
+    },
+    {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#5a5a5a" }]
+    },
+    {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#111827" }]
+    },
+    {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [{ color: "#242424" }]
+    }
+];
---
+++ MAPA/wwwroot/js/mapInterop.js
@@ -0,0 +1,51 @@
+﻿window.c2aMap = {
+    initMap: function (mapId, centerLat, centerLng, zoom, points) {
+        // Si el mapa ya existe, lo limpiamos (por si recargas el componente)
+        if (window.c2aMap._map) {
+            window.c2aMap._map.remove();
+            window.c2aMap._map = null;
+        }
+
+        var map = L.map(mapId).setView([centerLat, centerLng], zoom);
+        window.c2aMap._map = map;
+
+        //---mapa normal
+        //L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        //    maxZoom: 19
+        //}).addTo(map);
+        //modod oscuro culero
+        //L.tileLayer(
+        //    'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+        //    { maxZoom: 19 }
+        //).addTo(map);
+
+
+        //-modo oscuro2
+        //L.tileLayer(
+        //    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        //    { maxZoom: 19 }
+        //).addTo(map);
+
+        //Estilo no oficial de google
+        L.tileLayer(
+            'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+            { maxZoom: 19 }
+        ).addTo(map);
+
+
+
+
+
+        L.marker([22.1565, -100.9855]).addTo(map)
+            .bindPopup('Ubicación central.')
+            .openPopup();
+        //if (points && points.length > 0) {
+        //    points.forEach(function (p) {
+        //        var marker = L.marker([p.lat, p.lng]).addTo(map);
+        //        if (p.label) {
+        //            marker.bindPopup(p.label);
+        //        }
+        //    });
+        
+    }
+};
